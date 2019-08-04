2c538541a007651813d0706bd0541c20dd0210fc
czartur <49949732+czartur@users.noreply.github.com>
2019-08-04 13:37:14 -0300

    template added from ime++

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define pb push_back
+#define mp make_pair
+#define cl(x,y) memset(x, y, sizeof(x))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+
+int main(){
+
+}
