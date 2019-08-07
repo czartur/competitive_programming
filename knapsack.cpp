5753c1ad382aae599ff92a2c9a09b77a2638c447
czar <arturalves.ca@gmail.com>
2019-08-06 23:41:28 -0300

    corrected

--- knapsack.cpp
+++ knapsack.cpp
@@ -7,6 +7,7 @@ using namespace std;
 //      inserting objects with size and
 //      value in a fixed capacity bag
 
+//set dp to -1
 //Col -> size used, Row -> item number
 int dp[N][N], wt[N], val[N];
 int func(int i, int j){
@@ -14,5 +15,6 @@ int func(int i, int j){
     if(j<wt[i]) return dp[i][j]=0;
     else return dp[i][j]=val[i];
   }
+  if(dp[i][j]!=-1) return dp[i][j];
   return dp[i][j]=max(func(i-1, j), val[i]+func(i-1, j-wt[i]));
 }
