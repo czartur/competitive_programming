8c166788f2e5d76c58050b6a4d1c76d2518a7ecf
czar <arturalves.ca@gmail.com>
2019-08-06 23:22:49 -0300

    knapsack added

---
+++ knapsack.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define N 100
+
+//Knapsack problem
+//GOAL: returns maximum total value while
+//      inserting objects with size and
+//      value in a fixed capacity bag
+
+//Col -> size used, Row -> item number
+int dp[N][N], wt[N], val[N];
+int func(int i, int j){
+  if(i==1){
+    if(j<wt[i]) return dp[i][j]=0;
+    else return dp[i][j]=val[i];
+  }
+  return dp[i][j]=max(func(i-1, j), val[i]+func(i-1, j-wt[i]));
+}
