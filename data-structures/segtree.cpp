69204432e00d5717ba921afed2ba1b8d7b1846f3
czar <arturalves.ca@gmail.com>
2019-08-11 18:19:19 -0300

    segtree added

---
+++ data-structures/segtree.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Segment Tree
+//build O(n), update O(log(n)), query O(log(n))
+//example base: sum of a segment
+
+#define x 2*p
+#define y 2*p+1
+#define mid (l+r)/2
+
+int st[4*N], v[N];
+
+void build(int p, int l, int r){
+  if(l==r) {st[p]=v[l]; return;}
+  build(x, l, mid), build(y, mid+1, r);
+  st[p]=st[x]+st[y];
+}
+
+void update(int p, int l, int r, int idx, int val){
+  if(l==r) {st[p]+=val; return;}
+  if(idx<l || idx > r) return;
+  update(x, l, mid), update(y, mid+1, r);
+  st[p]=st[x]+st[y];
+}
+
+int query(int p, int l, int r, int i, int j){
+  if(l>=i && r<=j) return st[p];
+  if(l>j || r<i) return 0; //here you must return sometihng neutral to the query result
+  return st[p]=query(x, l, mid, i, j) + query(y, mid+1, r, i, j); //it could min, max..
+}
