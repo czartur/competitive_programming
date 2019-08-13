99862e7cab821fcf9174ce3ae4601782502fe8ef
czar <arturalves.ca@gmail.com>
2019-08-13 09:05:11 -0300

    -else

--- dp/lcs.cpp
+++ dp/lcs.cpp
@@ -8,5 +8,5 @@ string a, b;
 int lcs(int i, int j){ //string lenghts considered
   if(!i || !j) return dp[i][j]=0;
   if(dp[i][j]==-1) return dp[i][j];
-  else if(a[i-1]==b[j-1]) return dp[i][j]=1+lcs(i-1, j-1);
+  if(a[i-1]==b[j-1]) return dp[i][j]=1+lcs(i-1, j-1);
   return dp[i][j]=max(lcs(i-1,j), lcs(i,j-1));
