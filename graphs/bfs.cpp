9fbc379031c858c20afb47e6b0e9c644ca3302fb
czar <arturalves.ca@gmail.com>
2019-08-13 08:53:45 -0300

    bfs, corrected

--- graphs/bfs.cpp
+++ graphs/bfs.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //Breadth First Search (BFS)
 //pode ser usado para medir distâncias
-//(arrau de distâncias setado em infinito ao invés do array de visitados)
+//(array de distâncias setado em infinito ao invés do array de visitados)
 
 #define N 100
 typedef vector<int> vi;
@@ -10,11 +10,11 @@ typedef vector<int> vi;
 int vis[N];
 vi adj[N]; //lista de adjacência
 
-void bfs(int u){
+void bfs(int x){
   queue <int> q;
   q.push(x);
   while(!q.empty()){
-    u=q.front();
+    int u=q.front();
     vis[u]=1;
     q.pop();
     for(auto v: adj[u]) if(!vis[v]) q.push(v);
