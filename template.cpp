bea75e3ffe49564b1e37c47f7627937f9bfb7ab1
czartur <49949732+czartur@users.noreply.github.com>
2023-01-22 12:13:43 +0100

    add db / dbs and const

--- template.cpp
+++ template.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
 #define st first
 #define nd second
 #define pb push_back
 #define mp make_pair
 #define cl(x,y) memset(x, y, sizeof(x))
-#define inf 0x3f3f3f3f
-#define linf 0x3f3f3f3f3f3f3f3f
 #define endl '\n'
 
 typedef long long ll;
@@ -15,7 +15,12 @@ typedef pair<int, int> pii;
 typedef pair<int, pii> piii;
 typedef vector<int> vi;
 
+const int inf = 0x3f3f3f3f;
+const ll linf = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1e9+7;
+const int N =1e5+5;
+
 int main(){
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 }
