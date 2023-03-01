04e9938ff774c63761799ac0724b352186fe556d
czartur <arturalves.ca@gmail.com>
2023-03-01 14:57:39 +0100

    fix inv count

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -11,7 +11,7 @@ vector <int> merge(vector <int> &v, int i, int j){
   vl=merge(v, i, (i+j)/2), vr=merge(v, (i+j)/2+1, j);
   int l=0, r=0;
   while(l<vl.size() || r<vr.size()){
-    if(r == vr.size() || (l<vl.size() && r<vr.size() && vl[l]<vr[r])) ans.pb(vl[l++]);
+    if(r == vr.size() || (l<vl.size() && r<vr.size() && vl[l]<vr[r])) ans.push_back(vl[l++]);
     else ans.push_back(vr[r++]), inv+=vl.size() - l;
   }
   return ans;
